b091d38c31c06a4d58140574cd948d13a8c09258
Lucas P <lucas.pavloff@gmail.com>
2020-04-02 12:22:21 -0400

    first commit

---
+++ You Are Awsome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awsome
+//
+//  Created by Evgueni Pavlov on 4/1/20.
+//  Copyright © 2020 Evgueni Pavlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
